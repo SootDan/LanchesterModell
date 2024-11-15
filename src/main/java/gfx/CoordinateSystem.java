ee63aa0bd80d5b67262b3d23eae9841bffce0f36
Daniel B. <124447854+SootDan@users.noreply.github.com>
2024-11-16 00:34:47 +0100

    feat: graph pop start calculation

--- src/main/java/gfx/CoordinateSystem.java
+++ src/main/java/gfx/CoordinateSystem.java
@@ -1,5 +1,6 @@
 package gfx;
 
+import app.Constants;
 import lanchester.Population;
 
 import javax.swing.*;
@@ -7,30 +8,21 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class CoordinateSystem extends JPanel {
-    int width;
-    int height;
+    int width, height;
+    Population G, H;
 
     ArrayList<Vector2D> coordXAxis = new ArrayList<>();
     ArrayList<Vector2D> coordYAxis = new ArrayList<>();
+    Vector2D coordOrigin, coordBounds;
 
-    Population G;
-    Population H;
-
-    double minX = 0;
     double maxX;
-
-    int border = 25;
-    int coordArrow = 5;
-
-    Vector2D coordOrigin;
-    Vector2D coordBounds;
+    int border = 25, coordArrow = 5;
 
 
     public CoordinateSystem(Population G, Population H) {
         this.G = G;
         this.H = H;
         maxX = Math.max(G.number, H.number);
-
     }
 
 
@@ -44,8 +36,26 @@ public class CoordinateSystem extends JPanel {
         coordOrigin = new Vector2D(border, border);
         coordBounds = new Vector2D(width - border, height - border);
 
+        drawAxes(g);
         drawCoordinateSystem(g);
+        drawTimer(g);
         drawPopGraph(g);
+    }
+
+
+    /**
+     * Draws the axes and their arrows.
+     */
+    private void drawAxes(Graphics g) {
+        // X-Axis
+        g.drawLine(coordOrigin.x, coordBounds.y, coordBounds.x, coordBounds.y);
+        g.drawLine(coordBounds.x - coordArrow, coordBounds.y - coordArrow, coordBounds.x, coordBounds.y);
+        g.drawLine(coordBounds.x, coordBounds.y, coordBounds.x - coordArrow, coordBounds.y + coordArrow);
+
+        // Y-Axis
+        g.drawLine(coordOrigin.x, coordOrigin.y, coordOrigin.x, coordBounds.y);
+        g.drawLine(coordOrigin.x - coordArrow, coordOrigin.y + coordArrow, coordOrigin.x, coordOrigin.y);
+        g.drawLine(coordOrigin.x, coordOrigin.y, coordOrigin.x + coordArrow, coordOrigin.y + coordArrow);
     }
 
 
@@ -53,39 +63,27 @@ public class CoordinateSystem extends JPanel {
      * Draws the coordinate system including arrows.
      */
     public void drawCoordinateSystem(Graphics g) {
-        g.setColor(Color.BLACK);
-        g.drawLine(coordOrigin.x, coordBounds.y, coordBounds.x, coordBounds.y);
-        g.drawLine(coordOrigin.x, coordOrigin.y, coordOrigin.x, coordBounds.y);
-
-        // Arrow x-axis
-        g.drawLine(coordBounds.x - coordArrow, coordBounds.y - coordArrow, coordBounds.x, coordBounds.y);
-        g.drawLine(coordBounds.x, coordBounds.y, coordBounds.x - coordArrow, coordBounds.y + coordArrow);
-
-        // Arrow y-axis
-        g.drawLine(coordOrigin.x - coordArrow, coordOrigin.y + coordArrow, coordOrigin.x, coordOrigin.y);
-        g.drawLine(coordOrigin.x, coordOrigin.y, coordOrigin.x + coordArrow, coordOrigin.y + coordArrow);
-
         // Set five dimensions on the labelled x-axis
         int incrementer = (coordBounds.y - coordOrigin.y) / 6;
         double popSize = maxX / 5;
         g.drawString("p", coordOrigin.x, coordOrigin.y - coordArrow);
 
-        for (int i = 1; i <= 5; i++) {
-            coordXAxis.add(new Vector2D(coordOrigin.x, coordOrigin.y + incrementer * i));
-            g.drawLine(coordXAxis.get(i - 1).x - coordArrow, coordXAxis.get(i - 1).y,
-                    coordXAxis.get(i - 1).x + coordArrow, coordXAxis.get(i - 1).y);
-            g.drawString(String.format("%.0f", maxX - (i - 1) * popSize),
-                    coordXAxis.get(i - 1).x - coordArrow * 5, coordXAxis.get(i - 1).y + coordArrow);
+        for (int i = 0; i <= 5; i++) {
+            coordYAxis.add(new Vector2D(coordOrigin.x, coordBounds.y + -incrementer * i));
+            g.drawLine(coordYAxis.get(i).x - coordArrow, coordYAxis.get(i).y,
+                    coordYAxis.get(i).x + coordArrow, coordYAxis.get(i).y);
+            g.drawString(String.format("%.0f", i * popSize),
+                    coordYAxis.get(i).x - coordArrow * 5, coordYAxis.get(i).y + coordArrow);
         }
 
         // Set ten dimensions on the labelled y-axis
         incrementer = (coordBounds.x - coordOrigin.x) / 11;
         g.drawString("p(t)", coordBounds.x + coordArrow, coordBounds.y);
-        for (int i = 1; i <= 10; i++) {
-            coordYAxis.add(new Vector2D(coordOrigin.x + incrementer * i, coordBounds.y));
-            g.drawLine(coordYAxis.get(i - 1).x, coordYAxis.get(i - 1).y - coordArrow,
-                    coordYAxis.get(i - 1).x, coordYAxis.get(i - 1).y + coordArrow);
-            g.drawString(30 * i +" Min", coordYAxis.get(i - 1).x, coordYAxis.get(i - 1).y  + coordArrow * 3);
+        for (int i = 0; i <= 10; i++) {
+            coordXAxis.add(new Vector2D(coordOrigin.x + incrementer * i, coordBounds.y));
+            g.drawLine(coordXAxis.get(i).x, coordXAxis.get(i).y - coordArrow,
+                    coordXAxis.get(i).x, coordXAxis.get(i).y + coordArrow);
+            g.drawString(30 * i +" Min", coordXAxis.get(i).x, coordXAxis.get(i).y  + coordArrow * 3);
         }
     }
 
@@ -93,14 +91,26 @@ public class CoordinateSystem extends JPanel {
     /**
      * Draws the population over time.
      */
-    public void drawPopGraph(Graphics g) {
+    private void drawPopGraph(Graphics g) {
         // TODO: Actually calculate how the graphs would look
-        g.setColor(Color.PINK);
-        g.drawLine(coordYAxis.getLast().x, coordYAxis.getLast().y,
-                coordXAxis.getFirst().x, coordXAxis.getFirst().y);
+        double startG = 1.0 - G.number / maxX;
+        double startH = 1.0 - H.number / maxX;
 
+        // G
+        g.setColor(Color.PINK);
+        g.drawLine(coordXAxis.getFirst().x, (int) (
+                        (double) (coordYAxis.getFirst().y - coordYAxis.getLast().y) * startG) + coordYAxis.getLast().y,
+                coordXAxis.getLast().x, coordYAxis.getFirst().y);
+
+        // H
         g.setColor(Color.ORANGE);
-        g.drawLine(coordYAxis.get(8).x, coordYAxis.get(8).y,
-                coordXAxis.get(1).x, coordXAxis.get(1).y);
+        g.drawLine(coordXAxis.getFirst().x, (int) (
+                (double) (coordYAxis.getFirst().y - coordYAxis.getLast().y) * startH) + coordYAxis.getLast().y,
+                coordXAxis.getLast().x, coordYAxis.getFirst().y);
+    }
+
+    private void drawTimer(Graphics g) {
+        // TODO: Add time system
+        g.drawString("Time Elapsed: 0 Minutes", coordXAxis.get(8).x, coordArrow * 2);
     }
 }
